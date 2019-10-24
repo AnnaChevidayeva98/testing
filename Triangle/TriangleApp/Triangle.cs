39bf75568a4776f95cc246214e7103591a89700a
Anna Chevidayeva <Anna-mulelka@yandex.ru>
2019-10-24 19:42:34 +0300

    -fixing

---
+++ Triangle/TriangleApp/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleApp
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(double storona1, double storona2, double storona3)
+        {
+            return (storona1 > 0 && storona2 > 0 && storona3 > 0 && (storona1 + storona2 >= storona3) && (storona1 + storona3 >= storona2) && (storona2 + storona3 >= storona1));
+        }
+    }
+}
---
+++ Triangle/TriangleTests/TriangleTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleApp;
+
+namespace TriangleTests
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void IsRightTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2, 3, 4));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossible()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(15, 6, 1));
+        }
+
+        [TestMethod]
+        public void IsTriangle_ReturnsTrue()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(3, 1, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleEquilateralTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 5, 5));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossible_SumMoreThanSideLength()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(3, 2, 5));
+        }
+
+        [TestMethod]
+        public void OneSideIsNegative()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-3, 1, 5));
+        }
+
+        [TestMethod]
+        public void OneSideIsZero()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 1, 5));
+        }
+
+        [TestMethod]
+        public void AllSidesIsNegative()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-1, -1, -5));
+        }
+
+        [TestMethod]
+        public void IsTriangleIsoscelesTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2, 2, 4));
+        }
+
+        [TestMethod]
+        public void AllSidesIsZero()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 0, 5));
+        }
+    }
+}
