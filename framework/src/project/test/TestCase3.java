5d92d07a39e14f665802f80b07174c28ad756f90
Anna Chevidayeva <Anna-mulelka@yandex.ru>
2020-01-21 03:09:36 +0300

    fix

--- framework/src/project/test/TestCase3.java
+++ framework/src/project/test/TestCase3.java
@@ -12,6 +12,6 @@ public class TestCase3 extends Base {
         Assert.assertTrue(mainPage.panel.isDisplayed(), "Main page did not opened");
         mainPage.languagesMenu.click();
         mainPage.ruLang.click();
-        Assert.assertEquals(mainPage.textCityFrom.getText(),"I?","Translation is not full");
+        Assert.assertEquals(mainPage.textCityFrom.getText(),"Iš","Translation is not full");
     }
 }
